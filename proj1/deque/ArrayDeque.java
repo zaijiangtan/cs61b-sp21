aacc579794ecf708b4fecb045ea2aa75d6a83413
zaijiangtan <tanyi13333@163.com>
2024-10-12 15:31:03 +0800

    project1 -Checkpoint

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,103 @@
+package deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> {
+    private T[] array;
+    private int size;
+    private int front;
+    private int rear;
+
+    ArrayDeque(){
+        array = (T[]) new Object[8];
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+
+    private void resize(int newSize){
+        T[] newArr = (T[]) new Object[newSize];
+        for(int i = 0; i < size; i++){
+            newArr[i] = array[i];
+            array[i] = null;
+        }
+        array = newArr;
+    }
+
+    public void addFirst(T item){
+        if(size == array.length){
+            resize(size * 2);
+        }
+
+        array[front] = item;
+        front = (front + array.length - 1) % array.length;
+        size += 1;
+    }
+
+    public void addLast(T item){
+        if(size == array.length){
+            resize(size * 2);
+        }
+
+        array[rear] = item;
+        rear = (rear + 1) & array.length;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        if(!isEmpty()){
+            while(front != rear){
+                System.out.print(array[front] + " ");
+                front = (front + 1) % array.length;
+            }
+        }
+    }
+
+    public T removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+
+        front = (front + 1) % array.length;
+        T item = array[front];
+        array[front] = null;
+        size -= 1;
+        return item;
+    }
+
+    public T removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+
+        rear = (rear + array.length - 1) % array.length;
+        T item = array[rear];
+        array[rear] = null;
+        size -= 1;
+        return item;
+    }
+
+    public T get(int index){
+        return array[index];
+    }
+
+//    public Iterator<T> iterator(){
+//
+//    }
+//
+//    public boolean equals(Object o){
+//
+//    }
+
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,126 @@
+package deque;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class LinkedListDeque<T> {
+    private class Node{
+        T data;
+        Node next;
+        Node prev;
+
+        public Node(T item){
+            data = item;
+            next = null;
+            prev = null;
+        }
+    }
+
+    private Node front;
+    private Node rear;
+    private int size;
+
+    LinkedListDeque(){
+        front.next = rear;
+        front.prev = null;
+        rear.prev = front;
+        rear.next = null;
+        front.data = null;
+        rear.data = null;
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        Node newNode = new Node(item);
+        newNode.next = front.next;
+        front.next.prev = newNode;
+        newNode.prev = front;
+        front.next = newNode;
+
+        size += 1;
+    }
+
+    public void addLast(T item){
+        Node newNode = new Node(item);
+        newNode.next = rear;
+        rear.prev.next = newNode;
+        newNode.prev = rear.prev;
+        rear.prev = newNode;
+
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if(front.next == rear){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        Node p = front.next;
+        while(p != rear){
+            System.out.print(p.data + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst(){
+        if(isEmpty()) {
+            return null;
+        }
+        T item = front.next.data;
+        front.next.data = null;
+        front.next.next.prev = front;
+        front.next = front.next.next;
+        return item;
+    }
+
+    public T removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+        T item = rear.prev.data;
+        rear.prev.data = null;
+        rear.prev.prev.next = rear;
+        rear.prev = rear.prev.prev;
+        return item;
+    }
+
+    public T get(int index){
+        if(isEmpty()){
+            return null;
+        }
+        Node current = front.next;
+        while(current != rear && index != 0){
+            current = current.next;
+            index -= 1;
+        }
+        return current.data;
+    }
+
+    public T getRecursive(int index){
+        if(isEmpty()){
+            return null;
+        }
+
+        if(index == 0){
+            return front.next.data;
+        }
+
+        index -= 1;
+        return getRecursive(index);
+    }
+
+//    public Iterator<T> iterator(){
+//
+//    }
+//
+//    public boolean equals(Object o){
+//
+//    }
+}
