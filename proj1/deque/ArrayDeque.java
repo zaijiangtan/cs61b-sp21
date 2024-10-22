824d55e8af843949c58c5e8bed097228ec3391c3
zaijiangtan <tanyi13333@163.com>
2024-10-23 00:43:13 +0800

    project1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -36,7 +36,7 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     private void resize(int newSize) {
         T[] newArr = (T[]) new Object[newSize];
         for (int i = 0; i < size; i++) {
-            newArr[i] = removeFirst();
+            newArr[i] = get(i);
         }
         front = newSize - 1;
         rear = size;
